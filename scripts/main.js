9926964a73696b5f2d3f56b1b3560dbb74f09bf1
Linus Bergman <linusbergman@pm.me>
2021-03-23 22:10:44 +0300

    Add playRound function and user input with validation
    JS is mirrored in scripts/main.js and index.html for prototyping as I
    can't get main.js to load via live-server atm.
    
    TODO: Implement game of 5 rounds with score-keeping.

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,51 @@
 function computerPlay() {
     let play = Math.floor(Math.random() * 3);
     if (play == 0) {
-	return 'Rock';
+	return 'Rock'
     } else if (play == 1) {
-	return 'Paper';
+	return 'Paper'
     } else {
 	return 'Scissors'
     }
 
 }
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+	return 'That\'s a tie!'
+    } else if (playerSelection == 'Rock') {
+	if (computerSelection == 'Scissors') {
+	    return `You Win! ${playerSelection} beats ${computerSelection}`
+	} else {
+	    return `You Lose! ${computerSelection} beats ${playerSelection}`
+	}
+    } else if (playerSelection == 'Paper') {
+	if (computerSelection == 'Rock') {
+	    return `You Win! ${playerSelection} beats ${computerSelection}`
+	} else {
+	    return `You Lose! ${computerSelection} beats ${playerSelection}`
+	} 
+    
+    } else {
+	if (computerSelection == 'Paper') {
+	    return `You Win! ${playerSelection} beats ${computerSelection}`
+	} else {
+	    return `You Lose! ${computerSelection} beats ${playerSelection}`
+
+	}
+    }
+}
+
+let playerSelection;
+let validPlay = false;
+while (validPlay == false) {
+    playerSelection = prompt('Rock, paper or scissors?');
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()
+    if (playerSelection == 'Rock' || playerSelection == 'Paper' || playerSelection == 'Scissors') {
+	validPlay = true;
+    }
+}
+
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
